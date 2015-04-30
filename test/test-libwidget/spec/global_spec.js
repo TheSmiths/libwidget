117a4c80228b5bc0cd588b638592baeeccb2e3fe
Matthias Benkort <matthias.benkort@gmail.com>
2015-04-30 16:57:34 +0200

    Prepare bases for testing

---
+++ test/test-libwidget/spec/global_spec.js
@@ -0,0 +1,12 @@
+describe("Testing general functionnality and error handling of the libwidget module. v0.2.0", function () {
+    //var Alloy = {
+        //createController: jasmine.createSpy('createControllerSpy'),
+        //createWidget: jasmine.createSpy('createWidgetSpy')
+    //};
+
+    var Alloy = require('alloy'); 
+    
+    it("Correctly override Alloy createWidget and createController method", function () {
+        expect(true).toBe(true);
+    });
+});
